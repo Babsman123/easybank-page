ffd72efca2ec7cf53fd6a5996b04607a1f956ec8
TundeAdelani <babspropertyconsult@gmail.com>
2025-05-17 22:58:39 +0100

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //HEADER INTERSECTION
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
-const menu = [...document.querySelectorAll(".menu")];
+// const menu = [...document.querySelectorAll(".menu")];
 const navLinks = document.querySelector(".nav__links");
 const menuOpen = document.querySelector(".menu__icon--open");
 const menuClose = document.querySelector(".menu__icon--close");
+
+const allSections = document.querySelectorAll(".section");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -26,24 +28,48 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
-// menu.forEach((menu) => {
-//   menu.addEventListener("click", () => {
-//     // console.log(navLinks);
-//     header.classList.toggle("header-mobile");
-//     // navLinks.classList.add("nav__links--open");
+//REVEAL SECTIONS
+const revealSection = function (entries, observer) {
+  console.log(entries);
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
 
-//     if ((menuClose.style.display = "block")) {
-//       navLinks.style.transform = `translateX(0)`;
-//       console.log("hello");
-//     }
-//   });
-// });
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
 
 menuOpen.addEventListener("click", () => {
   header.classList.toggle("header-mobile");
   navLinks.style.transform = `translateX(0)`;
 });
+
 menuClose.addEventListener("click", () => {
   header.classList.toggle("header-mobile");
   navLinks.style.transform = `translateX(150%)`;
+
+  // After the animation finishes, reset to -150% to prepare for next open
+  navLinks.addEventListener("transitionend", function resetPosition() {
+    navLinks.style.transition = "none";
+    navLinks.style.transform = `translateX(-200%)`;
+
+    void navLinks.offsetWidth;
+
+    navLinks.style.transition = "transform 0.4s ease";
+
+    header.classList.remove("header-mobile");
+
+    // Remove the event listener after running once
+    navLinks.removeEventListener("transitionend", resetPosition);
+  });
 });
